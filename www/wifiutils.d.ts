d43c601fc5972b335e7418ba2a7b7efb86d45a46
Andreas Scherer <t3asdev@googlemail.com>
2017-02-16 10:44:25 +0100

    added ts definitions

---
+++ www/wifiutils.d.ts
@@ -0,0 +1,37 @@
+interface Cordova {
+    exec(success: (data: any) => any, fail: (err: any) => any, service: string, action: string, args?: any[]): void;
+}
+declare var cordova: Cordova;
+declare var module: any;
+interface Window {
+    wifiutils: WifiUtils;
+}
+declare type wifiConnectionState = 'CONNECTING' | 'CONNECTED' | 'SUSPENDED' | 'DISCONNECTING' | 'DISCONNECTED' | 'UNKNOWN';
+declare type wifiApState = 'WIFI_AP_STATE_DISABLING' | 'WIFI_AP_STATE_DISABLED' | 'WIFI_AP_STATE_ENABLING' | 'WIFI_AP_STATE_ENABLED' | 'WIFI_AP_STATE_FAILED';
+interface IWifiInfos {
+    connected: boolean;
+    apEnabled: boolean;
+    wifiConnected: boolean;
+    wifiState: wifiConnectionState | wifiApState;
+    supplicantState: string;
+    BSSID: string;
+    HiddenSSID: boolean;
+    SSID: string;
+    MacAddress: string;
+    AndroidHostname: string;
+    RSSI: number;
+    LinkSpeed: number;
+    activeAdaper: string;
+    addresses: Address[];
+}
+interface Address {
+    addressType: 'ipv4' | 'ipv6';
+    ipAddress: string;
+    subnetMask: string;
+    networkId: string;
+    broadcastAddress: string;
+}
+declare class WifiUtils {
+    init(successCallback: () => void, errorCallback: (error: string) => void): void;
+    getInfos(successCallback: (wifiInfos: IWifiInfos) => void, errorCallback: (error: string) => void): void;
+}
--- www/wifiutils.js
+++ www/wifiutils.js
@@ -1,12 +1,12 @@
-var exec = cordova.require('cordova/exec');
-
-module.exports = {
-    getInfos: function (successCb, errorCb) {
-        exec(successCb, // success
-             errorCb, // failure
-             'WifiUtils',
-             'getInfos',
-             []
-        );
+var WifiUtils = (function () {
+    function WifiUtils() {
     }
-};
+    WifiUtils.prototype.init = function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'AsaLivestream', 'start', []);
+    };
+    WifiUtils.prototype.getInfos = function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'AsaLivestream', 'start', []);
+    };
+    return WifiUtils;
+}());
+module.exports = new WifiUtils();
